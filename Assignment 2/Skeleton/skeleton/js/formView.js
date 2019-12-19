e84e9967c0a82eabbed07b02e31a075d6896dd3a
Adeline <asoe0002@student.monash.edu>
2019-12-19 21:30:25 +1100

    Feature 1 - 3. Not Finished

--- Assignment 2/Skeleton/skeleton/js/formView.js
+++ Assignment 2/Skeleton/skeleton/js/formView.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function clearForm(){
+    
+}
+
+function saveForm(){
+    let roomNumber, address, lightsOn, heatingCoolingOn, seatUsed, seatTotal, timeChecked;
+    document.getElementById("roomNumber").value = roomNumber;
+    document.getElementById("address").value= address;
+    document.getElementById("lights").value = lightsOn;
+    console.log(roomNumber, address, lightsOn);
+    
+}
--- Assignment 2/Skeleton/skeleton/js/shared.js
+++ Assignment 2/Skeleton/skeleton/js/shared.js
@@ -1,1 +1,43 @@
 "use strict";
+
+class RoomUsage
+{
+    constructor(roomNumber, address, lightsOn, heatingCoolingOn, seatUsed, seatTotal, timeChecked) {
+        if (typeof(roomNumber) === "string" && roomNumber !== ""){
+            this._roomNumber = roomNumber;
+        }
+        if (typeof(address) === "string" && address !== ""){
+            this._address = address;
+        }
+        if (typeof(lightsOn) === 'boolean'){
+            this._lightsOn = lightsOn;
+        }
+        
+        if (typeof(heatingCoolingOn)==="boolean"){
+            this._heatingCoolingOn = heatingCoolingOn;
+        }
+        if (typeof(seatUsed)==="number" && seatUsed >0 && seatUsed < seatTotal){
+            this._seatsUsed = seatUsed;
+        }
+        
+        if(typeof(seatTotal) === "number" && seatTotal > 0){
+            this._seatsTotal = seatTotal;
+        }
+        this._timeChecked = timeChecked;
+        
+    }
+
+}
+
+class RoomUsageList
+{
+    constructor()
+    {
+        this._roomList = [];
+    }
+    
+    addRoomUsage(roomUsage){
+        roomList.push(roomUsage);
+    }
+
+}
