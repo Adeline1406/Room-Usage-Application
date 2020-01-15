8192ecf22e7338001c52398d1af968b19b9f2831
Michael <mabd0027@student.monash.edu>
2020-01-15 11:10:00 +1100

    Fixed showObservations function
    Removed parentheses from all class function calls within that function (eg. roomUsageInstance.getLightsOn() -> roomUsageInstance.getLightsOn) and updated time to be equal to roomUsageInstance.timeChecked

--- Assignment 2/Skeleton/skeleton/js/observations.js
+++ Assignment 2/Skeleton/skeleton/js/observations.js
@@ -10,9 +10,8 @@ showObservations(exampleRoom);
 
 
 function showObservations(roomUsageInstance){
-    
     //convert the time into a string
-    let time = roomUsageInstance.getTime()
+    let time = roomUsageInstance.timeChecked
     let amOrPm = " am";
     let hours = time.getHours();
     if ( hours > 12){
@@ -29,13 +28,13 @@ function showObservations(roomUsageInstance){
     let dateString = date +" "+ monthString[month];
     
     
-    let lights = roomUsageInstance.getLightsOn();
+    let lights = roomUsageInstance.getLightsOn;
     let lightsOnOff ="";
     
     if (lights === true){lightsOnOff = "On";}
     else {lightsOnOff = "Off";}
     
-    let heatCool= roomUsageInstance.getHeatingCoolingOn();
+    let heatCool= roomUsageInstance.getHeatingCoolingOn;
     let heatCoolOnOff = "";
     
     if (heatCool === true){heatCoolOnOff= "On";}
@@ -44,12 +43,12 @@ function showObservations(roomUsageInstance){
 
     let newObservation =
         "<div class=\"mdl-cell mdl-cell--4-col\"><table class=\"observation-table mdl-data-table mdl-js-data-table mdl-shadow--2dp\"><thead><tr><th class=\"mdl-data-table__cell--non-numeric\"><h4 class=\"date\">" + dateString + "</h4><h4>" + 
-            roomUsageInstance.getAddress()+"<br />"+ 
-            "Rm "+ roomUsageInstance.getRoomNumber()+ "</h4></th></tr></thead><tbody><tr><td class=\"mdl-data-table__cell--non-numeric\">"+
+            roomUsageInstance.getAddress+"<br />"+ 
+            "Rm "+ roomUsageInstance.getRoomNumber+ "</h4></th></tr></thead><tbody><tr><td class=\"mdl-data-table__cell--non-numeric\">"+
             "Time: "+ timeString +"<br />"+
             "Lights: " + lightsOnOff + "<br />"+
             "Heating/cooling: "+ heatCoolOnOff+"<br />"+
-            "Seat usage: " + roomUsageInstance.getSeatsUsed() + " / "+ roomUsageInstance.getSeatsTotal()+ "<br/ >"
+            "Seat usage: " + roomUsageInstance.getSeatsUsed + " / "+ roomUsageInstance.getSeatsTotal + "<br/ >"
         "<button class=\"mdl-button mdl-js-button mdl-button--icon\" onclick=\"deleteObservationAtIndex(237);\"><i class=\"material-icons\">delete</i></button></td></tr></tbody></table></div>";
 
     let content = document.getElementById("content");
