3b69ad1523ba85e016ec46654f9993038a364994
Michael <mabd0027@student.monash.edu>
2020-01-17 15:47:20 +1100

    Working on F12

--- Assignment 2/Skeleton/skeleton/js/buildingStats.js
+++ Assignment 2/Skeleton/skeleton/js/buildingStats.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//Retrieve  the data of roomUsageList from storage
+
+//var roomUsageListStorage = retrieveRoomUsage();
+var roomUsageListStorage = testDataFunc();
+
+
+if (roomUsageListStorage !== null) {
+    for (let index = 0; index < roomUsageListStorage.arrayLength ; index++) {
+        roomUsageListStorage.roomUsageInstance(index).decodeJSONTimeChecked();
+    }
+} else {
+    console.log("roomUsageListStorage is null!")
+}
+
+
+let building = roomUsageListStorage.aggregateBy(roomUsageInstanceList.building);
+console.log(building)
+
+for (let prop in building) {
+    console.log(building[prop]._roomList)
+    let 
+}
+
+
